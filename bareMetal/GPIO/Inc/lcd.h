5de79007feb39b817e697a1ff033860e92b0f2a4
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2023-04-18 00:12:03 -0600

    Update lcd.h

--- bareMetal/GPIO/Inc/lcd.h
+++ bareMetal/GPIO/Inc/lcd.h
@@ -12,35 +12,35 @@
 /* Completar la lista de definicion de pines del LCD (Puerto B)            */
 
 #define LCD_RS_PIN_HIGH			GPIO_BSRR_Bxxx_Pos//		Set pin RS_LCD
-#define LCD_RS_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin RS_LCD
-#define LCD_RW_PIN_HIGH 		GPIO_BSRR_Bxxx_Pos//		Set pin RW_LCD 
-#define LCD_RW_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin RW_LCD 
-#define LCD_EN_PIN_HIGH 		GPIO_BSRR_Bxxx_Pos//		Set pin EN_LCD 
-#define LCD_EN_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin EN_LCD 
+#define LCD_RS_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin RS_LCD
+#define LCD_RW_PIN_HIGH 	GPIO_BSRR_Bxxx_Pos//		Set pin RW_LCD 
+#define LCD_RW_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin RW_LCD 
+#define LCD_EN_PIN_HIGH 	GPIO_BSRR_Bxxx_Pos//		Set pin EN_LCD 
+#define LCD_EN_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin EN_LCD 
 
-#define LCD_D4_PIN_HIGH			GPIO_BSRR_Bxxx_Pos//		Set pin DATA4_LCD 
-#define LCD_D5_PIN_HIGH 		GPIO_BSRR_Bxxx_Pos//		Set pin DATA5_LCD 
-#define LCD_D6_PIN_HIGH 		GPIO_BSRR_Bxxx_Pos//		Set pin DATA6_LCD 
-#define LCD_D7_PIN_HIGH 		GPIO_BSRR_Bxxx_Pos//		Set pin DATA7_LCD 
+#define LCD_D4_PIN_HIGH		GPIO_BSRR_Bxxx_Pos//		Set pin DATA4_LCD 
+#define LCD_D5_PIN_HIGH 	GPIO_BSRR_Bxxx_Pos//		Set pin DATA5_LCD 
+#define LCD_D6_PIN_HIGH 	GPIO_BSRR_Bxxx_Pos//		Set pin DATA6_LCD 
+#define LCD_D7_PIN_HIGH 	GPIO_BSRR_Bxxx_Pos//		Set pin DATA7_LCD 
 
-#define LCD_D4_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin DATA4_LCD 
-#define LCD_D5_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin DATA5_LCD 
-#define LCD_D6_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin DATA6_LCD 
-#define LCD_D7_PIN_LOW 			GPIO_BSRR_Bxxx_Pos//		Reset pin DATA7_LCD 
+#define LCD_D4_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin DATA4_LCD 
+#define LCD_D5_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin DATA5_LCD 
+#define LCD_D6_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin DATA6_LCD 
+#define LCD_D7_PIN_LOW 		GPIO_BSRR_Bxxx_Pos//		Reset pin DATA7_LCD 
 /* *********************************************************************** */
 
 //Definimos los nombres de los comandos para el LCD
-#define LCD_Clear( )			LCD_Write_Cmd( 0x01U )//	Borra la pantalla
+#define LCD_Clear( )			  LCD_Write_Cmd( 0x01U )//	Borra la pantalla
 #define LCD_Display_ON( )		LCD_Write_Cmd( 0x0EU )//	Pantalla LCD activa
-#define LCD_Display_OFF( )		LCD_Write_Cmd( 0x08U )//	Pantalla LCD inactiva
-#define LCD_Cursor_Home( )		LCD_Write_Cmd( 0x02U )//	Establecer el cursor a 'Home'
-#define LCD_Cursor_Blink( )		LCD_Write_Cmd( 0x0FU )//	Cursor intermitente
+#define LCD_Display_OFF( )	LCD_Write_Cmd( 0x08U )//	Pantalla LCD inactiva
+#define LCD_Cursor_Home( )	LCD_Write_Cmd( 0x02U )//	Establecer el cursor a 'Home'
+#define LCD_Cursor_Blink( )	LCD_Write_Cmd( 0x0FU )//	Cursor intermitente
 #define LCD_Cursor_ON( )		LCD_Write_Cmd( 0x0EU )//	Cursor visible activo
 #define LCD_Cursor_OFF( )		LCD_Write_Cmd( 0x0CU )//	Cursor inactivo
-#define LCD_Cursor_Left( )		LCD_Write_Cmd( 0x10U )//	Movimiento hacia la izquierda del cursor
-#define LCD_Cursor_Right( )		LCD_Write_Cmd( 0x14U )//	Movimiento hacia la derecha del cursor
-#define LCD_Cursor_SLeft( )		LCD_Write_Cmd( 0x18U )//	Movimiento hacia la izquierda de la pantalla
-#define LCD_Cursor_SRight( )	LCD_Write_Cmd( 0x1CU )//	Movimiento hacia la derecha de la pantalla
+#define LCD_Cursor_Left( )	LCD_Write_Cmd( 0x10U )//	Movimiento hacia la izquierda del cursor
+#define LCD_Cursor_Right( )	LCD_Write_Cmd( 0x14U )//	Movimiento hacia la derecha del cursor
+#define LCD_Cursor_SLeft( )	LCD_Write_Cmd( 0x18U )//	Movimiento hacia la izquierda de la pantalla
+#define LCD_Cursor_SRight( )LCD_Write_Cmd( 0x1CU )//	Movimiento hacia la derecha de la pantalla
 
 //Lista de funciones
 void LCD_Data_Out4(unsigned char val);
