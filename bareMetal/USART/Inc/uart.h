5dc25b5780e3123249104f2cafc92a33d124510e
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2024-04-03 12:08:24 -0600

    Update uart.h

--- bareMetal/USART/Inc/uart.h
+++ bareMetal/USART/Inc/uart.h
@@ -1,10 +1,10 @@
 #ifndef UART_H_
 #define UART_H_
 
-#define USARTDIV		 0x1D4C//				    9600 baud rate with a 72MHz of FPCLK
+#define USARTDIV  0x1D4C//				    9600 baud rate with a 72MHz of FPCLK
 #define USART_CR1_UE	( 0x1UL << 13U )
-#define USART_CR1_M		( 0x1UL << 12U )
-#define USART_CR1_TE( 0x1UL <<  3U )
+#define USART_CR1_M	( 0x1UL << 12U )
+#define USART_CR1_TE  ( 0x1UL <<  3U )
 #define USART_CR2_STOP	( 0x3UL << 12U )
 #define USART_SR_TXE	( 0x1UL <<  7U )
 
