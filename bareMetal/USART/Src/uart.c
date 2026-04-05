fc19af8d5b62719d5e2b84ee5d8ddbc3b90a36f0
Raul <78507802+Rahu7p@users.noreply.github.com>
2026-04-05 01:26:15 -0600

    Initialize USART1 with clock enable and settings

--- bareMetal/USART/Src/uart.c
+++ bareMetal/USART/Src/uart.c
@@ -5,11 +5,12 @@
 static void USER_USART1_Send_8bit( uint8_t Data );
 
 void USER_USART1_Init( void ){
-	USART1->CR1	|=	 USART_CR1_UE;//	Step 1 Usart enabled
-	USART1->CR1	&=	~USART_CR1_M;//		Step 2 8 Data bits
-	USART1->CR2	&=	~USART_CR2_STOP;//	Step 3 1 Stop bit
-	USART1->BRR	 =	 USARTDIV;//		Step 5 Desired baud rate
-	USART1->CR1	|= 	 USART_CR1_TE;//	Step 6 Transmitter enabled
+	RCC->APB2ENR	|=	 ( 0x1UL << 14U );//	USART 1 clock enable	
+	USART1->CR1		|=	 USART_CR1_UE;//		Step 1 Usart enabled
+	USART1->CR1		&=	~USART_CR1_M;//			Step 2 8 Data bits
+	USART1->CR2		&=	~USART_CR2_STOP;//		Step 3 1 Stop bit
+	USART1->BRR	 	|=	 USARTDIV;//			Step 5 Desired baud rate
+	USART1->CR1		|= 	 USART_CR1_TE;//		Step 6 Transmitter enabled
 }
 
 void USER_USART1_Transmit( uint8_t *pData, uint16_t size ){
