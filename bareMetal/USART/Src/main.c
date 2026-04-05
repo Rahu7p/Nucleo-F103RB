31f37b5414a2505523e32dc7779e708048f1305e
Raul <78507802+Rahu7p@users.noreply.github.com>
2026-04-05 01:27:45 -0600

    Implement system clock config and update GPIO init
    Added system clock configuration function and updated GPIO initialization.

--- bareMetal/USART/Src/main.c
+++ bareMetal/USART/Src/main.c
@@ -14,36 +14,41 @@
 /* Libraries, Definitions and Global Declarations */
 #include <stdint.h>
 #include "main.h"
-#include "uart.h"
+#include "user_uart.h"
 
 uint8_t msg[] = "Hello World!\r\n";
-
-/* Function prototypes */
-void USER_RCC_ClockEnable( void );
-void USER_GPIO_Init( void );
 
 /* Superloop structure */
 int main(void)
 {
 	/* Declarations and Initializations */
-	USER_RCC_ClockEnable( );
+	USER_SystemClock_Config( );
 	USER_GPIO_Init( );
 	USER_USART1_Init( );
-
     	/* Repetitive block */
     	for(;;){
     		USER_USART1_Transmit( msg, sizeof( msg ));
     	}
 }
 
-void USER_RCC_ClockEnable( void ){
-	RCC->APB2ENR	|=	 ( 0x1UL <<  2U )//	IO port A clock enable
-			|	 ( 0x1UL << 14U );//	USART 1 clock enable
-}
 void USER_GPIO_Init( void ){
+	RCC->APB2ENR	|=	 ( 0x1UL <<  2U );//	IO port A clock enable
 	//pin PA9 (USART1_TX) as alternate function output push-pull, max speed 10MHz
-	GPIOA->CRH	&=	~( 0x1UL <<  6U )
-			&	~( 0x2UL <<  4U );
-	GPIOA->CRH	|=	 ( 0x2UL <<  6U )
-			|	 ( 0x1UL <<  4U );
+	GPIOA->CRH		&=	~( 0x1UL <<  6U ) &	~( 0x2UL <<  4U );
+	GPIOA->CRH		|=	 ( 0x2UL <<  6U ) |	 ( 0x1UL <<  4U );
+}
+
+void USER_SystemClock_Config( void ){
+	FLASH->ACR	&=	~( 0x5UL <<  0U );//		two wait states latency, if SYSCLK > 48MHz
+	FLASH->ACR	|=	 ( 0x2UL <<  0U );//		two wait states latency, if SYSCLK > 48MHz
+	RCC->CFGR	&=	~( 0x1UL << 16U )//			PLL HSI oscillator clock /2 selected as PLL input clock
+				&	~( 0x7UL << 11U )// 		APB2 prescaler /1
+				&	~( 0x3UL <<  8U );// 		APB1 prescaler /2
+	RCC->CFGR	|=	 ( 0xFUL << 18U )//			PLL input clock x 16 (PLLMUL bits)
+				|	 ( 0x4UL <<  8U );//		APB1 prescaler /2
+	RCC->CR		|=	 ( 0x1UL << 24U );//		PLL2 ON
+	while( !(RCC->CR & ~( 0x1UL << 25U )));//	wait until PLL is locked
+	RCC->CFGR	&=	~( 0x1UL << 0U  );//		PLL used as system clock (SW bits)
+	RCC->CFGR	|=	 ( 0x2UL << 0U  );//		PLL used as system clock (SW bits)
+	while( 0x8UL != ( RCC->CFGR & 0xCUL ));//	wait until PLL is switched
 }
